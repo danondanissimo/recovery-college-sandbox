8d42b692fea51e4147d062226133c30df38247de
danondanissimo <danilievdanilo@gmail.com>
2026-08-05 15:27:10 +0100

    Removed unwanted files and created the structure and styles for the new website.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 
-import './js/modal.js';
-import './js/menu.js';
-import './js/checkbox-order.js';
-import './js/menu-close.js';
-import './js/scroll.js';
+// import './js/modal.js';
+// import './js/menu.js';
+// import './js/checkbox-order.js';
+// import './js/menu-close.js';
+// import './js/scroll.js';
 
+
+
+//       document.addEventListener('DOMContentLoaded', function() {
+//   const calendarEl = document.getElementById('calendar');
+
+//   const calendar = new FullCalendar.Calendar(calendarEl, {
+//     initialView: 'dayGridMonth',
+//     // You MUST include the plugin in the array for the options to be recognized
+//     plugins: [ FullCalendar.dayGridPlugin, FullCalendar.googleCalendarPlugin ],
+    
+//     // Now these options will be recognized
+//     googleCalendarApiKey: 'AIzaSyDMflHOLDHtHGBT0tKjbfFinChkcHx5PgI',
+//     events: {
+//       googleCalendarId: 'd3f770d364fe159140b1ff3d3465a77c7b302db99bec8df260b82d9e37281928@group.calendar.google.com'
+//     }
+//   });
+
+//   calendar.render();
+// });
