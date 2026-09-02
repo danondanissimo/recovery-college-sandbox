6c1094c14b31a92ffc8b3dfa9232d490aa620f00
danondanissimo <danilievdanilo@gmail.com>
2026-09-02 11:02:55 +0100

    Update website files

--- src/js/about.js
+++ src/js/about.js
@@ -1,8 +1,7 @@
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+
 
 function getYouTubeEmbedUrl(url) {
   if (!url) return '';
