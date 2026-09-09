26d70771c5f328a5cd440e468dfa7b3e7b71ac2f
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2026-09-09 11:15:45 +0000

    Deploying to gh-pages from @ danondanissimo/recovery-college-sandbox@cdc572118502671a26bb4eb07e9ed952d48902d1 🚀

--- assets/meetTheTeam-knRj9IOg.js
+++ assets/meetTheTeam-knRj9IOg.js
@@ -1,4 +1,4 @@
-import"./index-C0YL6EoF.js";async function o(){const e=document.getElementById("team-list");if(!e)return;e.innerHTML=`
+import"./index-D5cz5pLt.js";async function o(){const e=document.getElementById("team-list");if(!e)return;e.innerHTML=`
     <div class="news-spinner-container">
       <div class="news-spinner"></div>
     </div>
--- assets/news-CJSIfcON.js
+++ assets/news-CJSIfcON.js
@@ -1,4 +1,4 @@
-import"./index-C0YL6EoF.js";async function b(){const t=document.querySelector(".news-list");if(t){t.innerHTML=`
+import"./index-D5cz5pLt.js";async function b(){const t=document.querySelector(".news-list");if(t){t.innerHTML=`
     <div class="news-spinner-container">
       <div class="news-spinner"></div>
     </div>
